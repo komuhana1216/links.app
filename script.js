df7bf568cf742fa9d6f7c9440bb538be12bc0567
komuhana1216 <galleria0302@gmail.com>
2026-07-21 10:23:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,65 @@ document.querySelector(".container").style.transform=
 `rotateY(${x}deg) rotateX(${-y}deg)`;
 
 });
+
+const canvas=document.getElementById("particles");
+const ctx=canvas.getContext("2d");
+
+canvas.width=innerWidth;
+canvas.height=innerHeight;
+
+const particles=[];
+
+for(let i=0;i<90;i++){
+
+particles.push({
+
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+
+vx:(Math.random()-.5)*0.4,
+vy:(Math.random()-.5)*0.4,
+
+r:Math.random()*2+1
+
+});
+
+}
+
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+particles.forEach(p=>{
+
+ctx.beginPath();
+
+ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+
+ctx.fillStyle="#4d7fff";
+
+ctx.fill();
+
+p.x+=p.vx;
+p.y+=p.vy;
+
+if(p.x<0)p.x=canvas.width;
+if(p.x>canvas.width)p.x=0;
+
+if(p.y<0)p.y=canvas.height;
+if(p.y>canvas.height)p.y=0;
+
+});
+
+requestAnimationFrame(animate);
+
+}
+
+animate();
+
+window.addEventListener("resize",()=>{
+
+canvas.width=innerWidth;
+canvas.height=innerHeight;
+
+});
